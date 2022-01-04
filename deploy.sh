d9759e03b9078abc2c8450421999f1068541d0fa
Simon Warchol <simonwarchol@g.harvard.edu>
2022-01-04 18:47:04 -0500

    Deploying script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 docker build -t zootle .
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 337392631707.dkr.ecr.us-east-1.amazonaws.com
+docker tag zootle 337392631707.dkr.ecr.us-east-1.amazonaws.com/zootle
 docker push 337392631707.dkr.ecr.us-east-1.amazonaws.com/zootle
 docker pull 337392631707.dkr.ecr.us-east-1.amazonaws.com/zootle
 docker run -d -p 80:80 337392631707.dkr.ecr.us-east-1.amazonaws.com/zootle:latest
