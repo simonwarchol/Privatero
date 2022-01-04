6a890fc2d5c6a13593d0651b823cfc045f4bb5e7
Simon Warchol <simonwarchol@g.harvard.edu>
2022-01-04 18:34:02 -0500

    Authenticating

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 docker build -t zootle .
-aws ecr get-login-password | docker login --username AWS --password-stdin 337392631707.dkr.ecr.us-east-1.amazonaws.comdocker tag zootle 337392631707.dkr.ecr.us-east-1.amazonaws.com/zootle
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 337392631707.dkr.ecr.us-east-1.amazonaws.com
 docker push 337392631707.dkr.ecr.us-east-1.amazonaws.com/zootle
 docker pull 337392631707.dkr.ecr.us-east-1.amazonaws.com/zootle
 docker run -d -p 80:80 337392631707.dkr.ecr.us-east-1.amazonaws.com/zootle:latest
