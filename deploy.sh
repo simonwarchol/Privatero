71f41ab93e6a413911a810e077a94a47c4baf389
Simon Warchol <simonwarchol@g.harvard.edu>
2022-01-04 11:29:28 -0500

    Removing cd

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
-cd zootle
 docker build -t zootle .
 docker tag zootle 337392631707.dkr.ecr.us-east-1.amazonaws.com/zootle
 docker push 337392631707.dkr.ecr.us-east-1.amazonaws.com/zootle
