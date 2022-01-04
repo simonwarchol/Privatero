2897bba8208bcfb27e73f52e35182ead9479104c
Simon Warchol <simonwarchol@g.harvard.edu>
2022-01-04 11:20:49 -0500

    removing secure

--- assets/script.js
+++ assets/script.js
@@ -22,7 +22,7 @@ function generateLink() {
 function generateUrl() {
     const privateKey = document.getElementById('privateID').value;
     const groupID = document.getElementById('groupID').value;
-    const baseURL = 'https://www.zootle.me'
+    const baseURL = 'http://www.zootle.me'
     // const baseURL = 'http://127.0.0.1:5000'
     return `${baseURL}/bib?groupID=${groupID}&privateKey=${privateKey}`
 }
