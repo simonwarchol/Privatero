842181cbae64e82771e25f5002bd9794b0f13677
Simon Warchol <simonwarchol@g.harvard.edu>
2022-01-04 18:30:23 -0500

    fixing shell scripy

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,7 @@ docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 docker build -t zootle .
-aws ecr get-login-password | docker login --username AWS --password-stdin337392631707.dkr.ecr.us-east-1.amazonaws.com
-docker tag zootle 337392631707.dkr.ecr.us-east-1.amazonaws.com/zootle
+aws ecr get-login-password | docker login --username AWS --password-stdin 337392631707.dkr.ecr.us-east-1.amazonaws.comdocker tag zootle 337392631707.dkr.ecr.us-east-1.amazonaws.com/zootle
 docker push 337392631707.dkr.ecr.us-east-1.amazonaws.com/zootle
 docker pull 337392631707.dkr.ecr.us-east-1.amazonaws.com/zootle
 docker run -d -p 80:80 337392631707.dkr.ecr.us-east-1.amazonaws.com/zootle:latest
