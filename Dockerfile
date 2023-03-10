fad9f797fd65e3f47762d29fc6f8d85caff620ef
Simon Warchol <simonwarchol@g.harvard.edu>
2023-03-10 10:27:21 -0500

    Adding escaping of special characters

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM public.ecr.aws/lambda/python:3.9
+FROM public.ecr.aws/lambda/python:3.8
 WORKDIR /app
 COPY ./ /app
 RUN pip3 install -r requirements.txt
---
+++ deployment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+
+virtualenv venv --python=python3
+
+source venv/bin/activate
+
+
+pip install -r requirements.txt
+
+# Test Locally
+#sls wsgi serve
+
+# Redeploy severless function
+sls deploy
